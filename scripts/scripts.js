958bc2c979d6a06b88f89df2e9e418d6034cfb83
Vasiliy <vasiokpavluyk@gmail.com>
2021-12-16 16:30:55 +0200

    Block with quotes was created

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,3 +4,19 @@ window.onscroll = function(){
     let progressHeight = (window.pageYOffset / totalHeight) * 100;
     progress.style.height = progressHeight + "%";
 }
+
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 200,
+      modifier: 1,
+      slideShadows: true,
+    },
+    loop: true,
+});
